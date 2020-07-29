73b9c12a2d19ba7c3043b2b785146f0f31e32603
Shahin Al Kabir Mitul <mitul.cse@gmail.com>
2020-07-29 12:28:38 +0600

    Set up configaration

---
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+         services.AddCors(options =>
+         {
+             options.AddPolicy("CorsPolicy", builder =>
+             builder.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+         });
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+         services.Configure<IISOptions>(options =>
+         {
+
+         });
+    }
+}
